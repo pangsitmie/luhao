3699ad5cbc38c78a279e6500fd56610877b4117f
pangsitmie <66163611+pangsitmie@users.noreply.github.com>
2023-05-29 17:37:25 +0800

    update

---
+++ cloudprogrammingonline.com/src/components/copy/Copy.tsx
@@ -0,0 +1,22 @@
+import { Box, Typography, useTheme } from '@mui/material'
+import { tokens } from "../../theme";
+
+import './copy.css'
+
+const Copy = () => {
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
+
+
+    return (
+        <Box className='copy_container' sx={{ backgroundColor: colors.primary[400] }}>
+            <Typography sx={{ fontSize: "8px" }}>
+                Please use Chrome or Safari for the best experience
+            </Typography>
+            <br />
+            <Typography sx={{ color: colors.primary[400], fontSize: "1px" }}>Design by <a className="a_transparent" href="https://roundbytes.com">Round Bytes</a></Typography>
+        </Box>
+    )
+}
+
+export default Copy
